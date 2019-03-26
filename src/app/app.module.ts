af439de759505261faa56a5b82915f9fd6b5832e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-26 18:52:59 +0900

    [Update] Boardに情報を表示

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule} from '@angular/material';
+import { MatButtonModule,
+         MatCheckboxModule,
+         MatSidenavModule,
+         MatToolbarModule,
+         MatIconModule,
+         MatListModule,
+         MatCardModule,
+         MatTableModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HelloComponent } from './hello/hello.component';
 import { BoardComponent } from './board/board.component';
@@ -28,8 +35,20 @@ import {AppRouters} from './app.routes';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatTableModule,
     FlexLayoutModule,
     AppRouters
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
